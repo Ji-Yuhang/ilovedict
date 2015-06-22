009df782699bef02f1a12b0065852707f44e5e65
Ji-Yuhang <yuhang.silence@gmail.com>
2015-06-22 13:51:39 +0800

    add review strategy

--- ruby/test.rb
+++ ruby/test.rb
@@ -1,8 +1,43 @@
 #!/usr/bin/env ruby
-#require "awesome_print"
+require "awesome_print"
 
 require_relative "review"
+require_relative "shanbay"
 
-h = HowToReview.new
+wordList = []
+File.open("unknown.txt") do |io|
+    io.each_line do |line|
+        word = line.chomp
+        wordList.push word
+        break if wordList.size > 30
+    end
+end
 
+collection = Model::Collection.new
+wordList.each_with_index do |word, index|
+    c = Model::Card.new
+    c.cardId = index
+    c.front = word
+    data = Shanbay.shanbayword word
+    next if data.nil? 
+    pron = data["pron"]
+    next if pron.nil?
+    if !pron.empty?
+        pron =  "\t [ " + pron + " ]"
+        pron+= "\n"
+    end
+
+
+    c.back = pron + data["cn_definition"]["defn"]
+    c.detail = data["en_definition"]["defn"]
+    c.mp3 = data["us_audio"]
+    collection.addCard c
+end
+
+#reviewRecords = collection.reviewRecords
+strategy = Control::RandomReview.new
+strategy.setCollection collection
+
+show = View::ShowCard.new
+show.review(strategy)
 
