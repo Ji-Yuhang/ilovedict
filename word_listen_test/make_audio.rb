939dc8c034970dd802578b8a9beef7052050c036
Ji-Yuhang <yuhang.silence@gmail.com>
2016-05-17 17:11:54 +0800

    add cloze

--- word_listen_test/make_audio.rb
+++ word_listen_test/make_audio.rb
@@ -77,7 +77,81 @@ def main
   end
 end
 
-main
+def play_audio(audio_data)
+  t = Tempfile.new
+  t.write audio_data
+  t.close
+  #exec("mplayer " + t.path + " >/dev/null 2>&1") if fork.nil?
+  `mplayer #{t.path} >/dev/null 2>1`
+
+end
+def split_sentence(sentence)
+  sentence.split.map{|c| d = c.downcase; d = d.strip; d = d.gsub /[^\w]/, ''}.select{|w| !w.empty?}
+end
+
+def cloze(sentence, wordlist)
+  temp_sentence = sentence.clone
+  sentence_words = split_sentence temp_sentence
+  will_cloze_words = sentence_words.select do |word|
+    wordlist.include? word
+  end
+
+  will_cloze_words.each do |word|
+    temp_sentence.gsub! " "+word, " "+"_"*word.size  
+    temp_sentence.gsub! " "+word.capitalize, " "+"_"*word.size  
+    temp_sentence.gsub! word.capitalize, " "+"_"*word.size  
+  end
+
+  [temp_sentence,will_cloze_words]
+end
+def test_data
+  wordlist = []
+  wordlist += get_all_words_from_collins(4)
+  wordlist += get_all_words_from_collins(3)
+  #wordlist += get_all_words_from_collins(2)
+  #wordlist += get_all_words_from_collins(1)
+  count = Audio.count
+  list = Audio.find rand(count)
+  sentence = list.sentence
+  audio =  list.audio_data
+
+  s = cloze(sentence, wordlist)
+  s1, s2 = s
+  p s1
+  originS2= s2
+  #ap s2.join(",")
+  play_audio audio
+  ap "cloze ... "
+  
+  #w = gets
+  while !s2.empty?
+    w = gets
+    w.strip!
+    break if w =="Show"
+    if w == "Help"
+       ap `ruby /Users/jyd/vimrc/shanbay.rb #{s2[0]} only_english`
+    end
+    if s2.include? w
+      s2.delete w
+      wordlist.delete w
+      ap "✓ " 
+    else
+      ap "x => #{w}" 
+    end
+    s = cloze(sentence, wordlist)
+    s1, s2 = s
+    p s1
+
+    ap "cloze ... "
+    play_audio audio
+  end
+  ap originS2
+  ap sentence
+  play_audio audio
+end
+
+test_data
+#main
 
 
 
